9e82b78b9a4102d7f489ec74f833c2a7f42e63a4
Andrew Nguyen <andrewnguyen841@gmail.com>
2024-01-06 19:53:15 -0800

    update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,6 @@
     <Head>
         <Title>About</Title>
     </Head>
-    <p>This is a dev blog ported over from React.js + Next.js (original blog <a href="https://github.com/OtherAndrew/nextjs-blog">here</a>).</p>
-    <p>It uses Vue.js, Nuxt.js, and Tailwind CSS.</p>
+    <p>This is a dev blog ported over from React.js + Next.js (original blog <a href="https://github.com/OtherAndrew/nextjs-blog">here</a>). It uses Vue.js, Nuxt.js, and Tailwind CSS.</p>
+    <p>Andrew is a recent graduate from the University of Washington Tacoma with a B.S. in Computer Science. He is interested in web development.</p>
 </template>
