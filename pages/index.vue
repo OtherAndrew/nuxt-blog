bb0c666a09fcda7ac2694f4f507a5d0113dfc17f
Andrew Nguyen <andrewnguyen841@gmail.com>
2024-01-06 16:51:51 -0800

    add key to posts list

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@
         <section class="text-xl pt-px">
             <h2 class="text-2xl font-bold my-4 mx-0">Blog</h2>
             <ul class="list-none p-0 m-0">
-                <li v-for="post in blogPosts" class="pb-5">
+                <li v-for="post in blogPosts" class="pb-5" :key="post._path">
                     <img class="max-w-20 mr-4 inline-block align-middle"
                         v-if="post.coverImage" 
                         :src="post.coverImage"
