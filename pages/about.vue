9b140e2979b8beb86995c69cb5085578555d98d3
Andrew Nguyen <andrewnguyen841@gmail.com>
2024-01-11 13:48:33 -0800

    update about page

--- pages/about.vue
+++ pages/about.vue
@@ -17,7 +17,7 @@ useHead({
     <article>
         <h1 class="text-3xl font-extrabold my-4">About</h1>
         <p>
-            The purpose of this blog is to document my software development experiences. This blog uses Vue.js, Nuxt, and Tailwind CSS, and is based on <NuxtLink to="https://github.com/OtherAndrew/nextjs-blog">another blog</NuxtLink> I developed using the <NuxtLink to="https://nextjs.org/learn-pages-router/basics/create-nextjs-app">Next.js Page Router tutorial</NuxtLink>.
+            The purpose of this blog is to document my software development experiences. This blog is based on <NuxtLink to="https://github.com/OtherAndrew/nextjs-blog">another blog</NuxtLink> I developed using the <NuxtLink to="https://nextjs.org/learn-pages-router/basics/create-nextjs-app">Next.js Page Router tutorial</NuxtLink>. I developed this blog using Vue.js, Nuxt, and Tailwind CSS.
         </p>
         <p>
             Andrew is a recent graduate from the University of Washington Tacoma with a B.S. in Computer Science. He is an open-source enthusiast and an avid GNU/Linux user. His other interests include heritage menswear and welted footwear.
