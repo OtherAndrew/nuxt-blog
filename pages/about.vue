576946e0fa05ba28f70b04aedee62474d07c0be2
Andrew Nguyen <andrewnguyen841@gmail.com>
2024-01-10 23:42:13 -0800

    contain about page content within article tag

--- pages/about.vue
+++ pages/about.vue
@@ -14,11 +14,13 @@ useHead({
 </script>
 
 <template>
-    <div>
+    <article>
         <h1 class="text-3xl font-extrabold my-4">About</h1>
-
-        <p>The purpose of this blog is to document my software development experiences. This blog uses Vue.js, Nuxt, and Tailwind CSS, and is based on <NuxtLink to="https://github.com/OtherAndrew/nextjs-blog">another blog</NuxtLink> I developed using the <NuxtLink to="https://nextjs.org/learn-pages-router/basics/create-nextjs-app">Next.js Page Router tutorial</NuxtLink>.</p>
-
-        <p>Andrew is a recent graduate from the University of Washington Tacoma with a B.S. in Computer Science. He is an open-source enthusiast and an avid GNU/Linux user. His other interests include heritage menswear and welted footwear.</p>
-    </div>
+        <p>
+            The purpose of this blog is to document my software development experiences. This blog uses Vue.js, Nuxt, and Tailwind CSS, and is based on <NuxtLink to="https://github.com/OtherAndrew/nextjs-blog">another blog</NuxtLink> I developed using the <NuxtLink to="https://nextjs.org/learn-pages-router/basics/create-nextjs-app">Next.js Page Router tutorial</NuxtLink>.
+        </p>
+        <p>
+            Andrew is a recent graduate from the University of Washington Tacoma with a B.S. in Computer Science. He is an open-source enthusiast and an avid GNU/Linux user. His other interests include heritage menswear and welted footwear.
+        </p>
+    </article>
 </template>
