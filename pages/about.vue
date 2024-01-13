e80edb800af8f91ee96631e17443540f93820ae1
Andrew Nguyen <andrewnguyen841@gmail.com>
2024-01-12 22:26:02 -0800

    update about page

--- pages/about.vue
+++ pages/about.vue
@@ -17,10 +17,13 @@ useHead({
     <article>
         <h1 class="text-3xl font-extrabold my-4">About</h1>
         <p>
-            The purpose of this blog is to document my software development experiences. This blog is based on <NuxtLink to="https://github.com/OtherAndrew/nextjs-blog">another blog</NuxtLink> I developed using the <NuxtLink to="https://nextjs.org/learn-pages-router/basics/create-nextjs-app">Learn Next.js Pages Router tutorial</NuxtLink>. I developed this blog using Vue.js, Nuxt, and Tailwind CSS.
+            The purpose of this blog is to document my software development experiences.
         </p>
         <p>
-            Andrew is a recent graduate from the University of Washington Tacoma with a B.S. in Computer Science. He is a <NuxtLink to="https://en.wikipedia.org/wiki/Free_and_open-source_software">FOSS</NuxtLink> enthusiast and an avid GNU/Linux user. His other interests include heritage menswear and welted footwear.
+            This blog is based on <NuxtLink to="https://github.com/OtherAndrew/nextjs-blog">another blog</NuxtLink> I developed using the <NuxtLink to="https://nextjs.org/learn-pages-router/basics/create-nextjs-app">Learn Next.js Pages Router tutorial</NuxtLink>. I developed this blog using Vue.js, Nuxt, and Tailwind CSS. You can read more about how I created this blog <NuxtLink to="posts/nuxt-blog">here</NuxtLink>.
+        </p>
+        <p>
+            Andrew is a recent graduate from the University of Washington Tacoma with a B.S. in Computer Science. He is a <NuxtLink to="https://en.wikipedia.org/wiki/Free_and_open-source_software">FOSS</NuxtLink> enthusiast and an avid GNU/Linux user. His favorite programming language is JavaScript, but he is also comfortable with Java and SQL. His other interests include heritage menswear and welted footwear.
         </p>
     </article>
 </template>
