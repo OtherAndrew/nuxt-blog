e032cb98840611d3e193c2b3d4826b1734b6f025
Andrew Nguyen <andrewnguyen841@gmail.com>
2024-01-18 16:08:10 -0800

    change welted footwear link in about page to stitchdown blog

--- pages/about.vue
+++ pages/about.vue
@@ -23,7 +23,7 @@ useHead({
             This blog is based on <NuxtLink to="https://github.com/OtherAndrew/nextjs-blog">another blog</NuxtLink> I developed using the <NuxtLink to="https://nextjs.org/learn-pages-router/basics/create-nextjs-app">Learn Next.js Pages Router tutorial</NuxtLink>. I developed this blog using Vue.js, Nuxt, and Tailwind CSS. You can read more about how I created this blog <NuxtLink to="posts/nuxt-blog">here</NuxtLink>.
         </p>
         <p>
-            Andrew is a recent graduate from the University of Washington Tacoma with a B.S. in Computer Science. He is a <NuxtLink to="https://en.wikipedia.org/wiki/Free_and_open-source_software">FOSS</NuxtLink> enthusiast and an avid GNU/Linux user. His favorite programming language is JavaScript, but he is also comfortable with Java and SQL. His other interests include <NuxtLink to="https://almostvintagestyle.com">heritage menswear</NuxtLink> and <NuxtLink to="https://www.reddit.com/r/goodyearwelt/">welted footwear</NuxtLink>.
+            Andrew is a recent graduate from the University of Washington Tacoma with a B.S. in Computer Science. He is a <NuxtLink to="https://en.wikipedia.org/wiki/Free_and_open-source_software">FOSS</NuxtLink> enthusiast and an avid GNU/Linux user. His favorite programming language is JavaScript, but he is also comfortable with Java and SQL. His other interests include <NuxtLink to="https://almostvintagestyle.com">heritage menswear</NuxtLink> and <NuxtLink to="https://www.stitchdown.com">welted footwear</NuxtLink>.
         </p>
     </article>
 </template>
